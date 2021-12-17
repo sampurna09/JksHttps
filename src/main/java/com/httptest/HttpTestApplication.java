a0c83912c6b253c64eb6c5b4598453b45e29694a
sampurna <sampurna@sampurna-Lenovo-B50-80>
2021-12-17 20:58:49 +0545

    main

---
+++ src/main/java/com/httptest/HttpTestApplication.java
@@ -0,0 +1,13 @@
+package com.httptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/httptest/HttpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
